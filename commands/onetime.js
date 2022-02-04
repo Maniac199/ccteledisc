5d065450aaac2d347e03b6f61cb302aeadf05448
fireseal <mrandolph1999@gmail.com>
2022-02-04 15:00:41 -0600

    initial

--- commands/onetime.js
+++ commands/onetime.js
@@ -10,50 +10,51 @@ const onetime = (context) => {
     }
 
     billingDB.getConnection(async (err, con) => {
-    let theGuild = client.guilds.cache.find(g => g.id === mainServerID);
-    let validated = 0;
-    let invalid = 0;
-        if(theGuild) {
-            let theMembers = theGuild.members.cache;
-            let membersArray = [];
-            let looper = 0;
-            for await (let m of theMembers) {
-                //let theMem = client.users.cache.find(mid => mid.id === m.user.id);
-                if(looper === 0) {
-                    console.log(m.user);
-                    looper++;
-                }
+        let validated = 0;
+        let invalid = 0;
 
-            }
-            console.log('membersArray size: ' + membersArray.length);
-            message.reply('Found ' + membersArray.length + ' members in the server.');
-            for(let i = 0; i < membersArray.length; i ++) {
-                let valSub = mysql.format("SELECT * FROM pxg_wc_customer_lookup LEFT JOIN pxg_wc_order_product_lookup ON pxg_wc_customer_lookup.customer_id = pxg_wc_order_product_lookup.customer_id LEFT JOIN pxg_postmeta ON pxg_wc_order_product_lookup.order_id = pxg_postmeta.post_id WHERE post_id IN ( SELECT meta_value FROM pxg_postmeta WHERE post_id IN ( SELECT post_id FROM pxg_postmeta WHERE meta_key = ?) AND meta_key = ?) AND meta_key = ? AND meta_value = ?",
-                    [
-                        '_subscription_id',
-                        '_order_id',
-                        'discord',
-                        membersArray[i]
-                    ]);
-                console.log('MYSQL QUERY: ' +valSub);
-                con.query(valSub, async (err, subResults) => {
-                    if (err) {
-                        throw (err);
-                    }
-                    if (subResults.length > 0) {
-                        validated++;
-                        console.log('validated ' + membersArray[i]);
-                    }
-                    else {
-                        invalid++;
-                        console.log('not valid ' + membersArray[i]);
-                    }
-                });
-            }
+        let membersArray = [];
+        await memberList(membersArray, mainServerID, client);
+
+
+        console.log('membersArray size: ' + membersArray.length);
+        message.reply('Found ' + membersArray.length + ' members in the server.');
+        for(let i = 0; i < membersArray.length; i ++) {
+            let valSub = mysql.format("SELECT * FROM pxg_wc_customer_lookup LEFT JOIN pxg_wc_order_product_lookup ON pxg_wc_customer_lookup.customer_id = pxg_wc_order_product_lookup.customer_id LEFT JOIN pxg_postmeta ON pxg_wc_order_product_lookup.order_id = pxg_postmeta.post_id WHERE post_id IN ( SELECT meta_value FROM pxg_postmeta WHERE post_id IN ( SELECT post_id FROM pxg_postmeta WHERE meta_key = ?) AND meta_key = ?) AND meta_key = ? AND meta_value = ?",
+                [
+                    '_subscription_id',
+                    '_order_id',
+                    'discord',
+                    membersArray[i]
+                ]);
+            console.log('MYSQL QUERY: ' +valSub);
+            con.query(valSub, async (err, subResults) => {
+                if (err) {
+                    throw (err);
+                }
+                if (subResults.length > 0) {
+                    validated++;
+                    console.log('validated ' + membersArray[i]);
+                }
+                else {
+                    invalid++;
+                    console.log('not valid ' + membersArray[i]);
+                }
+            });
         }
+
         con.release();
         message.reply('validated ' + validated + ' members, ' + invalid + ' not validated');
     });
+}
+
+const memberList = (memArray, mainServerID, client) => {
+    let theGuild = client.guilds.cache.find(g => g.id === mainServerID);
+    let theMembers = theGuild.members.cache;
+    theMembers.forEach(mem => {
+        memArray.push(mem.id);
+    });
+    return memArray;
 }
 
 module.exports = onetime;
