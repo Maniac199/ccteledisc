9811b5abd156624b292b8698b1e4128e18c2838e
fireseal <mrandolph1999@gmail.com>
2022-02-04 14:03:58 -0600

    initial

--- database.js
+++ database.js
@@ -7,6 +7,7 @@ module.exports = (configuration) => {
     user: configuration.billingDatabaseUser,
     password: configuration.billingDatabasePassword,
     database: configuration.billingDatabaseName,
+    charset: "utf8mb4_unicode_520_ci",
   });
   const botDB = mysql.createPool({
     connectionLimit: 10,
@@ -14,6 +15,7 @@ module.exports = (configuration) => {
     user: configuration.botDatabaseUser,
     password: configuration.botDatabasePassword,
     database: configuration.botDatabaseName,
+    charset: "utf8mb4_unicode_520_ci",
   });
   return {billingDB, botDB};
 };
