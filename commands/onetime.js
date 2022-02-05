e0d7829ec26ca0ddffb59715ff7eddcc7c471497
fireseal <mrandolph1999@gmail.com>
2022-02-04 21:36:54 -0600

    added verify

--- commands/onetime.js
+++ commands/onetime.js
@@ -12,22 +12,22 @@ const onetime = async (context) => {
 
         let theGuild = client.guilds.cache.find(g => g.id === mainServerID);
         let theMembers = theGuild.members.cache;
-        for await (let tm of theMembers) {
-            let mtag = theGuild.members.cache.find(t => t.id === tm.id);
+        theMembers.forEach(mem => {
+
             let valSub = mysql.format("SELECT * FROM pxg_wc_customer_lookup LEFT JOIN pxg_wc_order_product_lookup ON pxg_wc_customer_lookup.customer_id = pxg_wc_order_product_lookup.customer_id LEFT JOIN pxg_postmeta ON pxg_wc_order_product_lookup.order_id = pxg_postmeta.post_id WHERE post_id IN ( SELECT meta_value FROM pxg_postmeta WHERE post_id IN ( SELECT post_id FROM pxg_postmeta WHERE meta_key = ?) AND meta_key = ?) AND meta_key = ? AND meta_value = ?",
                 [
                     '_subscription_id',
                     '_order_id',
                     'discord',
-                    mtag.user.tag
+                    mem.user.tag
                 ]);
             con.query(valSub, (err, subResults) => {
                 if (err) {
                     throw (err);
                 }
                 if (subResults.length > 0) {
-                    console.log('validated ' + mtag.user.tag);
-                    if(botCache.indexOf(mtag.id) > 0) {
+                    console.log('validated ' + mem.user.tag);
+                    if(botCache.indexOf(mem.id) > 0) {
                         console.log('Found in botCache, moving to next');
                     }
                     else {
@@ -35,8 +35,8 @@ const onetime = async (context) => {
                         botDB.getConnection(async (err, botcon) => {
                             let botIns = mysql.format('INSERT INTO discord (discord_tag, discord_id, order_id) VALUES (?, ?, ?)',
                                 [
-                                    mtag.user.id,
-                                    mtag.id,
+                                    mem.user.id,
+                                    mem.id,
                                     subResults[0].order_id
                                 ]);
                             botcon.query(botIns, (err, r) => {
@@ -44,15 +44,15 @@ const onetime = async (context) => {
                                     throw (err);
                                 }
                                 else {
-                                    botCache.push(mtag.id);
-                                    console.log(mtag.id + ' added to bot database');
+                                    botCache.push(mem.id);
+                                    console.log(mem.id + ' added to bot database');
                                 }
                             })
                         });
                     }
                 }
             });
-        }
+        });
         con.release();
         botcon.release();
     });
