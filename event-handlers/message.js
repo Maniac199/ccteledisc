f04b5f3d0d9cc1b492a28cb3b0122e34d13ccd92
fireseal <mrandolph1999@gmail.com>
2022-03-03 00:04:31 -0600

    added verify

--- event-handlers/message.js
+++ event-handlers/message.js
@@ -1,12 +1,12 @@
 const createMessageHandler = context => ctx => {
- // const { configuration, logger, teleClient } = context;
+  const { configuration, logger, teleClient } = context;
   //const { update } = ctx;
-  //const { mainServerID, botLogsChannel } = configuration;
+  const { mainServerID, botLogsChannel } = configuration;
   //const { text, chat, from } = message;
   console.log('message handler called');
   //console.log(ctx);
   //const chatID = chat.id;
-  ctx.reply('You said: ' + ctx.update.channel_post.text);
+  ctx.reply('You said: ' + ctx.update.channel_post.text + ' and botlogschannel is: ' + botLogsChannel);
 };
 
 module.exports = { createMessageHandler };
