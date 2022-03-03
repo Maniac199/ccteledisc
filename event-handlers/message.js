57df6c43d2aeb5a00462902c7d4f337c416e864d
fireseal <mrandolph1999@gmail.com>
2022-03-03 00:18:28 -0600

    added verify

--- event-handlers/message.js
+++ event-handlers/message.js
@@ -4,6 +4,7 @@ const createMessageHandler = context => ctx => {
   let theGuild = client.guilds.cache.find(g => g.id === mainServerID);
   let logChan = theGuild.channels.cache.find(c => c.id === botLogsChannel);
 
+
   console.log('message handler called');
   //console.log(ctx);
   //const chatID = chat.id;
