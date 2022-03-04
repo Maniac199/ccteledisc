9a2045635fc4d6af6aa85e857f62a5a06fbe55db
fireseal <mrandolph1999@gmail.com>
2022-03-04 12:35:08 -0600

    Final tests

--- event-handlers/message.js
+++ event-handlers/message.js
@@ -35,8 +35,7 @@ const createMessageHandler = context => ctx => {
       .setTimestamp()
       .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
-  logChan.send('testing');
-  //logChan.send({ embeds: [exampleEmbed] });
+  logChan.send({ embeds: [exampleEmbed] });
  // logChan.send({ embeds: [embedMsg] });
   //console.log({ embed: [embed] })
 };
