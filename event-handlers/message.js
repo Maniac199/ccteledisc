e0508ddc0535589f4e3a7969fefad16c22bae85d
fireseal <mrandolph1999@gmail.com>
2022-03-05 22:58:05 -0600

    added verify

--- event-handlers/message.js
+++ event-handlers/message.js
@@ -7,11 +7,11 @@ const createMessageHandler = context => ctx => {
   let theGuild = client.guilds.cache.find(g => g.id === mainServerID);
   let logChan = theGuild.channels.cache.find(c => c.id === botLogsChannel);
   let msgContent = ctx.update.channel_post.text;
-
+  let title = ctx.update.channel_post.chat.id.toString();
   const embedMsg = new MessageEmbed()
       .setColor(0x3498DB)
       .setAuthor({name: "CryptoCache"})
-      .setTitle(ctx.update.channel_post.chat.id)
+      .setTitle(title)
       .setDescription(msgContent)
       .setTimestamp();
   logChan.send({ embeds: [embedMsg] });
