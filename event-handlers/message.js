2d031fa6de20068dc0d86c1bef202222e31695ac
fireseal <mrandolph1999@gmail.com>
2022-03-05 20:01:29 -0600

    added verify

--- event-handlers/message.js
+++ event-handlers/message.js
@@ -15,7 +15,8 @@ const createMessageHandler = context => ctx => {
       .setDescription(msgContent)
       .setTimestamp();
   logChan.send({ embeds: [embedMsg] });
-  console.log(ctx);
+  console.log(ctx.update.channel_post.chat);
+  console.log(ctx.update.channel_post.sender_chat);
 
 };
 
