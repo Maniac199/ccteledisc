1ad0b35cb4e592ef667c2907f775ed1ec5dfff52
fireseal <mrandolph1999@gmail.com>
2022-04-12 12:01:09 -0500

    Final tests

--- commands/hello.js
+++ commands/hello.js
@@ -1,6 +1,6 @@
 
 const hello = context => ctx => {
-
+  console.log('entered hello');
   ctx.reply('Please respond with: $verify lookup email postcode/zipcode. For example:\\n$verify lookup billing@cryptocache.tech 12345');
 
 };
