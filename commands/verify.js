d7344f479b42c75da217b04ba64720454a732d08
fireseal <mrandolph1999@gmail.com>
2022-04-29 14:47:25 -0500

    Final tests

--- commands/verify.js
+++ commands/verify.js
@@ -143,9 +143,6 @@ const verify = (context) => {
                             if(err) {
                                 throw (err);
                             }
-                            else {
-                                botCache.push(theMem.id);
-                            }
                         });
                         botcon.release();
                     });
