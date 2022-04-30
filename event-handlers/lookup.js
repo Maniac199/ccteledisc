c9224418704763d062eb7be5929a2a8331287787
fireseal <mrandolph1999@gmail.com>
2022-04-30 16:54:04 -0500

    Final tests

--- event-handlers/lookup.js
+++ event-handlers/lookup.js
@@ -4,13 +4,17 @@ const createLookupHandler = context => ctx => {
   const { configuration } = context;
   const { botPrefix } = configuration;
   context.ctx = ctx;
-  context.args = ctx.update.message.text.slice(botPrefix.length).trim().split(' ');
-  const userCommandEntry = context.args.shift();
+  if(ctx.update.message.text) {
+    if (ctx.update.message.text.startsWith(botPrefix)) {
+      context.args = ctx.update.message.text.slice(botPrefix.length).trim().split(' ');
+      const userCommandEntry = context.args.shift();
 
-  const command = commandList.find((cmd) => cmd.name === userCommandEntry);
+      const command = commandList.find((cmd) => cmd.name === userCommandEntry);
 
-  if (command) {
-    command.execute(context);
+      if (command) {
+        command.execute(context);
+      }
+    }
   }
 
 };
