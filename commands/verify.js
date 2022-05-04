acea48facc41b850dfb519ff6492f80c28affaee
Maniac199 <mrandolph1999@gmail.com>
2022-05-03 21:45:35 -0500

    Updates

--- commands/verify.js
+++ commands/verify.js
@@ -129,7 +129,7 @@ const verify = (context) => {
                         member_limit: 1,
                         expire_date: date
                     }
-                    console.log(Date.now() + ' plus 1 hour: ' + date);
+                    //console.log(Date.now() + ' plus 1 hour: ' + date);
                     const premLink = await ctx.telegram.createChatInviteLink(teleprem, options);
                     const swingLink = await ctx.telegram.createChatInviteLink(teleswing, options);
                     logChan.send(ctx.message.from.username + ' used ' + ctx.message.text + ' and was granted access. Premium link: ' + premLink.invite_link + ' Swing link: ' + swingLink.invite_link);
