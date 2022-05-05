4581cf565453e3df2a1e8d1a1f4cca588d972e07
Maniac199 <mrandolph1999@gmail.com>
2022-05-04 20:51:07 -0500

    Updates

--- event-handlers/start.js
+++ event-handlers/start.js
@@ -1,7 +1,7 @@
 const createStartHandler = ctx => {
   if(!ctx.message.from.is_bot) {
     ctx.reply('Request acknowledged, I have started a private chat with you.');
-    ctx.telegram.sendMessage(ctx.message.from.id, "Hello, I am the verification bot for CryptoCache Premium. I will walk you through the verification process. Please respond with: $verify lookup email postcode/zipcode. For example:\n$verify lookup billing@cryptocache.tech 12345");
+    ctx.telegram.sendMessage(ctx.message.from.id, "Hello, I am the verification bot for CryptoCache Premium. I will walk you through the verification process. \n\nPlease respond with: $verify lookup email postcode/zipcode. \n\nFor example:\n$verify lookup billing@cryptocache.tech 12345");
   }
 };
 
