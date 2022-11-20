d6d177373795b28c945395ebf1960da23cb18e85
Maniac199 <mrandolph1999@gmail.com>
2022-11-20 10:20:56 -0600

    Updates

--- event-handlers/lookup.js
+++ event-handlers/lookup.js
@@ -2,21 +2,26 @@ const commandList = require('../command-list');
 
 const createLookupHandler = context => ctx => {
   const { configuration } = context;
-  const { botPrefix } = configuration;
+  const { botPrefix, botLogsChannel} = configuration;
   context.ctx = ctx;
-  if(ctx.update.message.text) {
-    if (ctx.update.message.text.startsWith(botPrefix)) {
-      context.args = ctx.update.message.text.slice(botPrefix.length).trim().split(' ');
-      const userCommandEntry = context.args.shift();
+  let logChan = theGuild.channels.cache.find(c => c.id === botLogsChannel);
+  try {
+    if (ctx.update.message.text) {
+      logChan.send(ctx.update.message.text);
+      if (ctx.update.message.text.startsWith(botPrefix)) {
+        context.args = ctx.update.message.text.slice(botPrefix.length).trim().split(' ');
+        const userCommandEntry = context.args.shift();
 
-      const command = commandList.find((cmd) => cmd.name === userCommandEntry);
+        const command = commandList.find((cmd) => cmd.name === userCommandEntry);
 
-      if (command) {
-        command.execute(context);
+        if (command) {
+          command.execute(context);
+        }
       }
     }
-  }
+  } catch (e) {
 
+  }
 };
 
 module.exports = { createLookupHandler };
